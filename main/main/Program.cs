5a857beb3305a95f29ebc8a9046c144c1046614c
williamleb <william.leb@hotmail.com>
2016-10-27 14:44:43 -0400

    Ajouter le meilleur code au monde.

---
+++ main/main/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Meilleur programme au monde.");
+      Console.Beep(2000, 50000);
+    }
+  }
+}
